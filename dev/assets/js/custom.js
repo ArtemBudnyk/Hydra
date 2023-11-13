f01823ff56681eeb80c39488d8cfb810dc3f9686
ArtemBudnyk <qrtleo@gmail.com>
2023-11-13 22:17:52 +0200

    header has bag

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,19 @@
+
+// function togglerNavbar () {
+   
+//     const navbarToggler = document.getElementById("nigga")
+
+//     var hasTestClass = navbarToggler.classList.contains('test')
+
+//     if (hasTestClass) {
+//         navbarToggler.classList.remove('test');
+//     } else {
+//         navbarToggler.classList.add('test');
+//     }
+// }
+
+
+
+
   
+
